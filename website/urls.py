93a6e266a6a993c270fce6c47c615236d404ef38
haiiamnikhil <haiiamnikhil@gmail.com>
2021-05-10 00:35:03 +0530

    Changed static configurations

--- website/urls.py
+++ website/urls.py
@@ -22,6 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app.urls')),
-] + static(settings.STATIC_URL,document_root=settings.STATICFILES_DIRS)
+] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
