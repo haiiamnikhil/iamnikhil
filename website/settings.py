f3cd6a5ade30c8b9e976da3f46d3cc26b5d87a8d
haiiamnikhil <haiiamnikhil@gmail.com>
2021-05-10 22:04:12 +0530

    added sitemap confg

--- website/settings.py
+++ website/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
