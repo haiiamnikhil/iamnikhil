f82bcf8e546fb9aead203ee1a6da938227c30fe5
haiiamnikhil <haiiamnikhil@gmail.com>
2021-05-24 16:48:02 +0530

    model changed

--- app/models.py
+++ app/models.py
@@ -12,6 +12,7 @@ class Resume(models.Model):
 
     def __str__(self):
         return self.fileName
+        
 
 class Projects(models.Model):
     slug = models.CharField(max_length=100,unique=True,blank=False,null=True)
@@ -22,7 +23,7 @@ class Projects(models.Model):
     projectDescription = models.TextField(max_length=500,blank=False,null=True)
     projectThumpnailImage = models.FileField(upload_to='Images/Project/Thumpnails/',null=True)
     projectDetailsImage = models.FileField(upload_to='Images/Project/Details/',null=True)
-    projectGithub = models.URLField(max_length=200,unique=False,null=True,blank=False)
+    projectGithub = models.URLField(max_length=200,unique=False,null=True,blank=True)
     uploadedDate = models.DateField(auto_now=True)
     updatedDate  = models.DateField(auto_now=True)
 
@@ -140,4 +141,4 @@ class Services(models.Model):
         return self.detailedHeading
 
     def get_absolute_url(self):
-        return reverse('servicesDetails',args=[self.slug])  
+        return reverse('servicesDetails',args=[self.slug])
